4b16aa4997702e97de363ef28cc2a237f5c76bb4
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2024-11-19 12:10:00 -0300

    Add header in App

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import './header.css'
+
+function Header() {
+    return (
+        <div className="container-header">
+            <h1>Sorteio de Times</h1>
+            <img src="img/logo-so-resenha.jpeg" alt='Logo Só Resenha FC'/>
+            <h1>Só Resenha FC</h1>
+        </div>
+    )
+}
+
+export default Header;
